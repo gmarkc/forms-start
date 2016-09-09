78104e01d7e5e8df3f88b91cf3e85644063a9c88
gmarkc <greg.cockerham@gmail.com>
2016-09-09 17:01:48 -0400

    Checkpoint

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -4,10 +4,20 @@ import { NgForm } from '@angular/forms';
 @Component({
   // moduleId: module.id,
   selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+  templateUrl: 'template-driven.component.html',
+  styles: [`
+    .ng-invalid {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateDrivenComponent {
+  user = {
+    username: 'Gregory',
+    email: 'greg.cockerham@gmail.com',
+    password: '43987984'
+  }
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(this.user);
   }
 }
